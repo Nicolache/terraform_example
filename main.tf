81f1831267f1cace9fbcf9b386d12c0392b2b803
nicolache <nik@host.domain>
2020-03-03 20:49:36 +0300

    No oldfashion variable usage anymore in main.tf. A static ip address is created for a compute instance.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  # project = "feisty-album-268710"
-  project = "${var.project}"
-  region  = "europe-west1"
+  project = var.project
+  # region  = "europe-west1"
+  region  = "europe-west6"
 }
 
 resource "google_compute_instance" "app" {
@@ -13,20 +13,21 @@ resource "google_compute_instance" "app" {
   boot_disk {
     initialize_params {
       # image = "reddit-base-1582587704"
-      image = "${var.disk_image}"
+      image = var.disk_image
     }
   }
   metadata = {
-    # sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
-    sshKeys = "appuser:${file("${var.public_key_path}")}"
+    # sshKeys = "appuser:${file("${var.public_key_path}")}"
+    sshKeys = "appuser:file(var.public_key_path)"
   }
   connection {
-    host        = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
+    # host        = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
+    host        = google_compute_instance.app.network_interface.0.access_config.0.nat_ip
     type        = "ssh"
     user        = "appuser"
     agent       = false
-    # private_key = "${file("~/.ssh/appuser")}"
-    private_key = "${file("${var.private_key_path}")}"
+    # private_key = "${file("${var.private_key_path}")}"
+    private_key = file(var.private_key_path)
   }
   provisioner "file" {
     source      = "files/puma.service"
@@ -37,8 +38,16 @@ resource "google_compute_instance" "app" {
     # сеть, к которой присоединить данный интерфейс
     network = "default"
     # использовать ephemeral IP для доступа из Интернет
-    access_config {}
+    access_config {
+      # nat_ip = "${google_compute_address.app_ip.address}"
+      nat_ip = google_compute_address.app_ip.address
+    }
   }
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+  region = "europe-west6"
 }
 
 resource "google_compute_firewall" "firewall_puma" {
@@ -56,3 +65,12 @@ resource "google_compute_firewall" "firewall_puma" {
   target_tags = ["reddit-app"]
 }
 
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
