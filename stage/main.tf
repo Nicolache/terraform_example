1060e136c3dbd3c732bdcf20cd28d5c3233a7b8d
nicolache <nik@host.domain>
2020-03-16 10:22:10 +0300

    a backend for tfstate file added.

--- stage/main.tf
+++ stage/main.tf
@@ -3,22 +3,25 @@ provider "google" {
   project = var.project
   region  = var.region
 }
-
 module "app" {
   source = "../modules/app"
   public_key_path = var.public_key_path
   app_disk_image = var.app_disk_image
 }
-
 module "db" {
   source = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image = var.db_disk_image
 }
-
 module "vpc" {
   source = "../modules/vpc"
   public_key_path = var.public_key_path
   source_ranges = ["0.0.0.0/0"]
   vpc_disk_image = var.vpc_disk_image
+}
+terraform {
+  backend "gcs" {
+    bucket = "devopscube-states"
+    prefix = "demo"
+  }
 }
